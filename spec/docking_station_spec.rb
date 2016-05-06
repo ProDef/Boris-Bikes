c180566ea729f13f6f3f49027506d0e9685db36a
Abdirazak Ali <prodef@live.com>
2016-05-06 22:39:37 +0100

    implementing instance variables to dock bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,11 +4,23 @@ describe DockingStation do
 	
 
 	it { is_expected.to respond_to :release_bike}
+	it { is_expected.to respond_to(:dock).with(1).argument }
+	it { is_expected.to respond_to (:bike)}
 
 	it "releases a working bike" do 
 		bike = subject.release_bike
 		expect(bike).to be_working
 	end
 
-	
+	it "docks something" do  
+		bike = Bike.new
+		expect(subject.dock(bike)).to eq bike
+	end
+
+	it "returns docked bike" do 
+		bike = Bike.new
+		subject.dock(bike)
+		expect(subject.bike).to eq bike
+	end
+
 end
