ae6376f01525c24bc797f5ae986007d171a254d9
Abdirazak Ali <prodef@live.com>
2016-05-07 19:43:31 +0100

    added exceptionsto not release infinite bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,8 +8,9 @@ describe DockingStation do
 	it { is_expected.to respond_to (:bike)}
 
 	it "releases a working bike" do 
-		bike = subject.release_bike
-		expect(bike).to be_working
+		bike = Bike.new
+		subject.dock(bike)
+		expect(subject.release_bike).to eq bike 
 	end
 
 	it "docks something" do  
@@ -21,6 +22,10 @@ describe DockingStation do
 		bike = Bike.new
 		subject.dock(bike)
 		expect(subject.bike).to eq bike
+	end
+
+	it "raises an error for no bikes" do
+		expect { subject.release_bike }.to raise_error("No bikes available")
 	end
 
 end
